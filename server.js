4cb9b0a0eba4c94528a4af01002b016dec1460ab
liannefm <lfmeiresonne@gmail.com>
2026-03-29 22:02:17 +0200

    node.js toegevoegd + styling konijnenscherm + styling controller

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+// DIT IS DE BELANGRIJKSTE REGEL
+app.use(express.static(__dirname));
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
