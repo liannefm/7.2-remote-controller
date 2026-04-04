35d37d8780ef41246b9875990ee7f67ba359a92d
liannefm <lfmeiresonne@gmail.com>
2026-04-04 17:22:06 +0200

    knoppen van de moodbar werkend

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
 
 // DIT IS DE BELANGRIJKSTE REGEL
 app.use(express.static(__dirname));
 
-app.listen(3000, () => {
+io.on('connection', (socket) => {
+  console.log('a user connected:', socket.id);
+
+  socket.on('increase-bar', (bar) => {
+    console.log('Received increase-bar for:', bar);
+    socket.broadcast.emit('increase-bar', bar);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
 });
